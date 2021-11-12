120cb69d9c2a47e26a8ec4cc74cc01c884e8d7bf
Bianca Torres <bnk13.01.97@gmail.com>
2021-11-12 00:38:27 -0500

    pipeline to deploy a web done

--- ubuntu/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pkill java
-
-nohup java -jar spring-petclinic/target/*.jar > spring-petclinic/output.log #!/bin/sh
-
-pkill java
-
-nohup java -jar spring-petclinic/target/*.jar > spring-petclinic/output.log &
